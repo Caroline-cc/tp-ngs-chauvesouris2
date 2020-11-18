d79038ab97aceb3620e659b93d5975abdff4511d
Caroline-cc <caroline.chauvie@ens-lyon.fr>
2020-11-18 23:12:26 +0000

    saving week 1

--- src/Trinity.sh
+++ src/Trinity.sh
@@ -1,24 +1,23 @@
 #! /bin/bash
 
-data="/ifb/data/mydatalocal"
+data="/ifb/data/mydatalocal/outputs"
 mkdir -p $data
 cd $data
 
 # Dossier doutput de trinity ()
-output_trinity="ifb/data/mydatalocal"
+output_trinity=$data/"output_trinity"
 mkdir -p $output_trinity
 cd $ouput_trinity 
 
 #Récupération des outputs de trimomatic
-home_trinity=$data/"dataclean"
+home_trinity=$data/"output_trimmomatic"
 leftseq=$(ls $home_trinity/*R1_paired.fastq.gz |paste -d "," -s)
 rightseq=$(ls $home_trinity/*R2_paired.fastq.gz |paste -d "," -s)
 echo $leftseq
 echo $rightseq
 
 
-#Pour exécuter faire ./nomfichier.sh
-#Si "permission denied" faire chmod u+x Trinity.sh
+
 
 #Commande trinity 
 
@@ -26,3 +25,6 @@ Trinity --seqType fq --max_memory 14G --left $leftseq --right $rightseq --CPU 4 
 
 
 ## fonction paste pour coller des chaînes de caractères
+## -d "," pour délimiter les séquences nucléotides des reads forward/reverse par des virgules
+## -s pour mettre toute la chaîne nucléotidique sur une seule ligne
+## le $  avant le ls indique qu'on va bien stocker le résultat dans la liste et pas le fichier
