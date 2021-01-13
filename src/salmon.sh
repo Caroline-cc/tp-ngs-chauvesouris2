b1714d3fbfebb7e9b87d9abd4989fe6097e88c35
Caroline-cc <caroline.chauvie@ens-lyon.fr>
2021-01-13 21:13:14 +0000

    Relecture f

--- src/salmon.sh
+++ src/salmon.sh
@@ -1,16 +1,16 @@
+#! /bin/bash
+# Transcripts quantification with Salmon (isoform-specific level)
 
-## Dossier par défaut
+## Default folder
+
 data="/home/rstudio/data/mydatalocal"
-
 cd $data
 
-## Création de l'index 
+## Create Salmon index
 
 salmon index -t $data/transfer_trinity/Trinity.fasta -i $data/outputs/output_index -p 4
 
 ## Quantification 
-
-###mkdir /home/rstudio/data/mydatalocal/data/salmon_alignment_paired_end
 
 for n in 1 2 3 4 5 6
 do 
